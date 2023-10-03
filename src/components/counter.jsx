fbd04d8d071c7aed07c7e83797215cebb5fe8291
Fatima <fawicoma@gmail.com>
2023-10-03 13:55:55 -0500

    Projects filter functionallity

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import recipe from '../assets/recipe-blog.png'
 import '../styles/counter.css'
 
@@ -10,12 +11,14 @@ const projects = [
     image : recipe,
   },{
     id: 1,
-    tags: ['HTML', 'CSS', 'responsive'],
+    tags: ['CSS', 'responsive'],
     title: 'Recipe Blog',
     description: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
     image : recipe,
   }
 ]
+
+const filters = ['HTML', 'CSS', 'responsive']
 
 function ProjectCounter(){
   return(
@@ -26,11 +29,28 @@ function ProjectCounter(){
 }
 
 function ProjectCounterFilter(){
+  const [filteredProjects, setFilteredProjects] = useState(projects)
+  
   return (
-    <div>
-
+    <div className='project-counter'>
+      <h2 className='title-4'>Project ({projects.length}) </h2>
+      <div>
+        { filters.map((filter) => {
+          let projectsFilter = projects.filter(project => project.tags.includes(filter) == true)
+          function handleFilter() {
+            setFilteredProjects(projectsFilter)
+          }
+          return(
+            <div key={filter}>
+              <label htmlFor="filter"> {filter}
+                <input type="radio" id={filter} name="filter" onChange={handleFilter}/>
+              </label>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
 
-export default ProjectCounter
+export default ProjectCounterFilter
